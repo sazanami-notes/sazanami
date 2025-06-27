6013c0a6feb8c942b956076ff841c169b12eb293
kaede.sato <main@kaedesato.work>
2025-06-27 17:12:52 +0900

    Tiptapを改造

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,3 +2,4 @@
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
 <h1 class="text-bold">かえでちゃんだよ</h1>
+<a href="/tiptap">tiptap</a>
--- src/routes/tiptap/+page.svelte
+++ src/routes/tiptap/+page.svelte
@@ -1,61 +1,68 @@
-<script>
-	import { onMount, onDestroy } from 'svelte';
-	import { Editor } from '@tiptap/core';
-	import StarterKit from '@tiptap/starter-kit';
 
+<script>
+  
+	import { Color } from '@tiptap/extension-text-style'
+	import { ListItem } from '@tiptap/extension-list'
+	import { TextStyle } from '@tiptap/extension-text-style'
+	import StarterKit from "@tiptap/starter-kit";
+	import { Editor } from "@tiptap/core";
+	import { onMount } from "svelte";
+  
 	let element;
 	let editor;
-
+  
 	onMount(() => {
-		editor = new Editor({
-			element: element,
-			extensions: [StarterKit],
-			content: '<p>Hello World! 🌍️ </p>',
-			onTransaction: () => {
-				// force re-render so `editor.isActive` works as expected
-				editor = editor;
-			}
-		});
+	  editor = new Editor({
+		element: element,
+		extensions: [
+		  Color.configure({ types: [TextStyle.name, ListItem.name] }),
+		  TextStyle.configure({ types: [ListItem.name] }),
+		  StarterKit,
+		],
+		content: `
+			  <h2>
+				Hi there,
+			  </h2>
+			  <p>
+				this is a <em>basic</em> example of <strong>Tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
+			  </p>
+			  <ul>
+				<li>
+				  That’s a bullet list with one …
+				</li>
+				<li>
+				  … or two list items.
+				</li>
+			  </ul>
+			  <p>
+				Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
+			  </p>
+			  <pre><code class="language-css">body {
+	display: none;
+  }</code></pre>
+			  <p>
+				I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
+			  </p>
+			  <blockquote>
+				Wow, that’s amazing. Good work, boy! 👏
+				<br />
+				— Mom
+			  </blockquote>
+			`,
+			editorProps: {
+      attributes: {
+        // ここにフォーカス時のスタイルを追加します
+        class: 'focus:outline-none',
+      },
+    },
+		onTransaction: () => {
+		  // force re-render so `editor.isActive` works as expected
+		  editor = editor;
+		},
+	  });
 	});
+  </script>
+  
 
-	onDestroy(() => {
-		if (editor) {
-			editor.destroy();
-		}
-	});
-</script>
-
-<div class="m-20 border-b">
-	{#if editor}
-		<button
-			on:click={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-			class:active={editor.isActive('heading', { level: 1 })}
-		>
-			H1
-		</button>
-		<button
-			on:click={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-			class:active={editor.isActive('heading', { level: 2 })}
-		>
-			H2
-		</button>
-		<button
-			on:click={() => editor.chain().focus().setParagraph().run()}
-			class:active={editor.isActive('paragraph')}
-		>
-			P
-		</button>
-	{/if}
-</div>
-
-<div bind:this={element} class="card bg-base-200 m-30 h-100 p-10" />
-
-<style>
-	button.active {
-		background: black;
-		color: white;
-	}
-	h1 {
-		font-size: large;
-	}
-</style>
+  <div bind:this={element} class="prose m-6 py-2 px-6 shadow-lg bg-gray-100" />
+  
