4064b14006fe68c416a000208ecd445d3cf92948
kaede.sato <main@kaedesato.work>
2025-07-22 18:25:58 +0900

    一旦コミット

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,7 @@
-// src/hooks.server.ts
-
 import { auth } from "$lib/server/auth";
 import { svelteKitHandler } from "better-auth/svelte-kit";
 import { building } from "$app/environment";
  
 export async function handle({ event, resolve }) {
-  // ライブラリ提供のハンドラに戻す
   return svelteKitHandler({ event, resolve, auth, building });
 }
--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,14 +1,23 @@
-import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+// src/lib/server/auth.ts
+
 import { betterAuth } from 'better-auth';
-import * as schema from '$lib/server/db/schema';
-
-import { BETTER_AUTH_SECRET } from '$env/static/private';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { sveltekitCookies } from 'better-auth/svelte-kit'; // [追加]
+import { getRequestEvent } from '$app/server'; // [追加]
 import { db } from '$lib/server/db';
+import * as schema from '$lib/server/db/schema';
+import { BETTER_AUTH_SECRET } from '$env/static/private';
 
-
-// D1のインスタンスを受け取って、authインスタンスを生成する関数を作成
 export const auth = betterAuth({
-		database: drizzleAdapter(db,{schema, provider: 'sqlite' }),
-		emailAndPassword: { enabled: true },
-		secret: BETTER_AUTH_SECRET || 'O5mYuDUIfa4su4GeYWz49Y52pv7capK9',
-	});
+	database: drizzleAdapter(db, {
+		schema: schema,
+		provider: 'sqlite'
+	}),
+	emailAndPassword: {
+		enabled: true
+	},
+	secret: BETTER_AUTH_SECRET,
+	// [追加] ここにpluginsセクションを追加します
+	plugins: [sveltekitCookies(getRequestEvent)]
+	
+});
