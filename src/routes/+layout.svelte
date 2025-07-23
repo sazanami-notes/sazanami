6574f417171e89b6214c5fb4cfb53d3ab1136c31
kaede.sato <main@kaedesato.work>
2025-07-23 14:54:57 +0900

    ミスってLayout消しちゃったので復活。

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+
+
+</script>
+
+<div class="bg-base-300 rounded-box mx-auto my-10 h-[80vh] w-2/3 shadow-xl">
+    {@render children()}
+</div>
