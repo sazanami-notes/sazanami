44689553b1380bf51feed9cc0f6079ca9ed3ce99
kaede.sato <main@kaedesato.work>
2025-07-29 18:53:13 +0900

    メニュー作った。

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,16 +1,15 @@
-// src/routes/+layout.server.ts
 import { auth } from '$lib/server/auth';
 import type { LayoutServerLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
 
 export const load: LayoutServerLoad = async ({ request, url }) => {
-	const session = await auth.api.getSession({
+	const sessionData = await auth.api.getSession({
 		headers: request.headers
 	});
 
-	if (!session && url.pathname !== '/login') {
+	if (!sessionData?.session && url.pathname !== '/login') {
 		throw redirect(303, '/login');
 	}
 
-	return { session };
+	return { session: sessionData?.session || null };
 };
